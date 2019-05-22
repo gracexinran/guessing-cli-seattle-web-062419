371d9e699549106a7753386b0578bc7ef6856124
Xinran Che <gracexinran@gmail.com>
2019-05-22 22:32:18 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -8,7 +8,7 @@ def run_guessing_game
   elsif input.to_i == rand 
     puts "You guessed the correct number!"
   else
-    puts "The computer guessed #{rand}"
+    puts "The computer guessed #{rand}."
   end
   
 end 
