657c4d8f0a4e3ade934e9f8a160f185db0460a9c
Xinran Che <gracexinran@gmail.com>
2019-05-22 21:53:56 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   random = rand(100)
   input = gets.chomp 
   until input.to_i == random 
-    puts "The computer guessed <number>."
+    puts "The computer guessed #{random}."
     input = gets.chomp 
     if input == "exit"
       return "exit" 
