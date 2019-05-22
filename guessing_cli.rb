ae78b174dd7450a3cfb0aafd0c4020b3acbd286e
Xinran Che <gracexinran@gmail.com>
2019-05-22 22:09:49 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -10,7 +10,7 @@ def run_guessing_game
       input = gets.chomp
     else
       puts "You guessed the correct number!"
-      input = "exit"
+      return "Goodbye!"
     end
   end
   puts "Goodbye!"
