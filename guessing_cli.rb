3b6e1a13ef2784c3ee547dc3d008a289fdc78e60
Xinran Che <gracexinran@gmail.com>
2019-05-22 22:36:52 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -6,9 +6,9 @@ def run_guessing_game
   if input == 'exit'
     puts "Goodbye!"
   elsif input.to_i == random 
-    return "You guessed the correct number!"
+    puts "You guessed the correct number!"
   else
-    return "The computer guessed #{random}."
+    puts "The computer guessed #{random}."
   end
 end 
 
